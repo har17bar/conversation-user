5933ce637ab8a4ecea7ba4654c7cbd1fc970d381
Harut <har17bar@gmail.com>
2020-07-26 16:00:53 +0300

    code style and formatting configuration

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,10 +1,10 @@
 import { BadRequestException, Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { UserService } from './user.service';
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class UserController {
+  constructor(private readonly appService: UserService) {}
 
   @MessagePattern('getHello')
   getHello() {
